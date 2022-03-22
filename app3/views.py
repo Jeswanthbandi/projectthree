5183c8913054c1daf51272a87ffb30bfbee5f5b7
Jeswanthbandi <nareshbabu779931@gmail.com>
2022-03-22 19:05:09 +0530

    first commit

---
+++ app3/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def function3(request):
+    return HttpResponse('this is my third project ')
+def function3(request):
+    return HttpResponse('this is sample')
